db139a9d342065c34eeaccfeddcf05f9b8331d57
BRomans <michele.romani.gzl0@gmail.com>
2021-08-26 17:23:52 +0200

    :sparkles: implemented method to send parameters to the Unreal GUI

--- persona_server/app.py
+++ persona_server/app.py
@@ -1,3 +1,4 @@
+import requests
 from flask import Flask, render_template, jsonify, request
 from flask_classful import FlaskView, route
 
@@ -7,11 +8,12 @@ app = Flask(__name__,
             static_folder='persona_vision_engine/static',
             template_folder='persona_vision_engine/static')
 
+persona_gui_addr = "http://localhost:5000/"
 
 class MyServer(FlaskView):
 
     def __init__(self):
-        self.request = None
+        self.received = None
 
     def index(self):  # put application's code here
         return render_template('persona.html')
@@ -21,9 +23,16 @@ class MyServer(FlaskView):
         # POST request
         if request.method == 'POST':
             print('Incoming..')
-            self.request = request.get_json()
-            print(self.request)  # parse as JSON
+            self.received = request.get_json() # parse as JSON
+            print(self.received['facing'])
             return 'OK', 200
+
+    def send_learned_parameter(self, learned):
+        # make a POST request
+        dictToSend = {'learned': learned}
+        res = requests.post(persona_gui_addr + "/learned", json=dictToSend)
+        print('response from server:', res.text)
+        dictFromServer = res.json()
 
 
 MyServer.register(app, route_base='/')
