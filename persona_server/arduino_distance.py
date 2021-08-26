d8b78eed5705b76146b98de350971f2a4169048f
BRomans <michele.romani.gzl0@gmail.com>
2021-08-26 17:24:14 +0200

    :bug: removed unnecessary while loop, tweaked parameters

--- persona_server/arduino_distance.py
+++ persona_server/arduino_distance.py
@@ -1,27 +1,26 @@
 import serial
 
-port = "/dev/tty.usbmodem1411"  # On Mac/Linux
-#port = "COM3"  # On Windows
+#port = "/dev/tty.usbmodem1411"  # On Mac/Linux
+port = "COM3"  # On Windows
 
 
 def return_arduino_distance(port):
     ser = serial.Serial(port)
-    while True:
-        string = ser.readline()
-        numbers = []
-        for word in string.split():
-            if word.isdigit():
-                numbers.append(int(word))
-        n = numbers[0]
-        if 0 <= n < 40:
-            reaction = 'd40'
-        elif 40 <= n < 75:
-            reaction = 'd75'
-        elif 75 <= n < 150:
-            reaction = 'd150'
-        else:
-            reaction = 'd200'
-        return reaction
+    string = ser.readline()
+    numbers = []
+    for word in string.split():
+        if word.isdigit():
+            numbers.append(int(word))
+    n = numbers[0]
+    if 0 <= n < 75:
+        reaction = 'd40'
+    elif 75 <= n < 150:
+        reaction = 'd75'
+    elif 150 <= n < 200:
+        reaction = 'd150'
+    else:
+        reaction = 'd200'
+    return reaction
 
 if __name__ == '__main__':
     while True:
